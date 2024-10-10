0bc719336212498eb10b051e0ba5858e3f7564aa
novalrnv <akuunoval@gmail.com>
2024-10-10 10:49:48 +0700

    Trial 2

--- SiakadFor12.java
+++ SiakadFor12.java
@@ -4,6 +4,7 @@ public class SiakadFor12{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int mahasiswalulus = 0, mahasiswatidaklulus = 0;
         
         for (int i = 1; i <= 10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -14,8 +15,15 @@ public class SiakadFor12{
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60){
+                mahasiswalulus++;
+            } else {
+                mahasiswatidaklulus++;
+            }
         }
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai tertinggi : " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + mahasiswalulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + mahasiswatidaklulus);
     }
 }
